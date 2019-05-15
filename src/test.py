9e4d02eaebaf45be8b51afe8ceeb2ffa20163ea5
akinariobi <yuliyanasher@gmail.com>
2019-05-16 00:46:02 +0300

    upd

--- src/test.py
+++ src/test.py
@@ -3,12 +3,13 @@ import librosa.display
 import matplotlib.pyplot as plt
 from dtw import dtw
 %matplotlib inline
+# rm later
 
-#Loading audio files
+# load audio files
 y1, sr1 = librosa.load('../samples/1.mp3')
 y2, sr2 = librosa.load('../samples/2.mp3')
 
-#Showing multiple plots using subplot
+# show multiple plots using subplot
 plt.subplot(1, 2, 1)
 mfcc1 = librosa.feature.mfcc(y1,sr1)   #Computing MFCC values
 librosa.display.specshow(mfcc1)
@@ -21,6 +22,6 @@ dist, cost, path = dtw(mfcc1.T, mfcc2.T)
 print("The normalized distance between the two : ",dist)   # 0 for similar audios
 
 plt.imshow(cost.T, origin='lower', cmap=plt.get_cmap('gray'), interpolation='nearest')
-plt.plot(path[0], path[1], 'w')   #creating plot for DTW
+plt.plot(path[0], path[1], 'w')   # create plot for DTW
 
-plt.show()  #To display the plots graphically
+plt.show()  # display the plots graphically
