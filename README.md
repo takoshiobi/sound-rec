ab722daf62e2c016c48f285712a76b2a8659340b
akinariobi <yuliyanasher@gmail.com>
2019-05-16 00:36:20 +0300

    calc

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
-### init
+### Mel-frequency cepstrum
 
-Python 3.x
+Compare two audio files. 
 
-### download
+### Download
 
 * `git clone https://github.com/akinariobi/sound-rec.git`
 * `cd sound-rec`
 
-### install & run
+### Install & Run
 
 * `pip install -r requirements.txt`
+* `python test.py` (from `src` folder)
 
+### Resources
+
+* https://en.wikipedia.org/wiki/Mel-frequency_cepstrum
+* https://mc.ai/how-i-taught-a-neural-network-to-understand-similarities-in-music-audio/
+* https://stackoverflow.com/questions/47543742/audio-recognition-and-comparison
+* https://stackoverflow.com/questions/38971969/how-to-compare-audio-on-similarity-in-python
+* https://www.researchgate.net/post/How_can_I_compare_the_frequency_spectra_of_two_different_audio_signals
---
+++ src/test.py
@@ -0,0 +1,25 @@
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+from dtw import dtw
+
+#Loading audio files
+y1, sr1 = librosa.load('../samples/1.mp3')
+y2, sr2 = librosa.load('../samples/2.mp3')
+
+#Showing multiple plots using subplot
+plt.subplot(1, 2, 1)
+mfcc1 = librosa.feature.mfcc(y1,sr1)   #Computing MFCC values
+librosa.display.specshow(mfcc1)
+
+plt.subplot(1, 2, 2)
+mfcc2 = librosa.feature.mfcc(y2, sr2)
+librosa.display.specshow(mfcc2)
+
+dist, cost, path = dtw(mfcc1.T, mfcc2.T)
+print("The normalized distance between the two : ",dist)   # 0 for similar audios
+
+plt.imshow(cost.T, origin='lower', cmap=plt.get_cmap('gray'), interpolation='nearest')
+plt.plot(path[0], path[1], 'w')   #creating plot for DTW
+
+plt.show()  #To display the plots graphically
